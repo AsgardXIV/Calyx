da7f3e920d5f4c77aa2ad6b8f62cffbe7d73b360
Asgard <git@asgard.io>
2025-04-01 00:17:11 -0600

    More repo stuff

--- src/sqpack/repository.zig
+++ src/sqpack/repository.zig
@@ -1,1 +1,34 @@
-pub const Repository = struct {};
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const SqPack = @import("sqpack.zig").SqPack;
+
+pub const Repository = struct {
+    const Self = @This();
+
+    allocator: Allocator,
+    pack: *SqPack,
+    repo_path: []const u8,
+    repo_id: u8,
+
+    pub fn init(allocator: std.mem.Allocator, pack: *SqPack, repo_path: []const u8, repo_id: u8) !*Self {
+        const self = try allocator.create(Self);
+        errdefer allocator.destroy(self);
+
+        const cloned_path = try allocator.dupe(u8, repo_path);
+        errdefer allocator.free(cloned_path);
+
+        self.* = Self{
+            .allocator = allocator,
+            .pack = pack,
+            .repo_path = cloned_path,
+            .repo_id = repo_id,
+        };
+
+        return self;
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.allocator.free(self.repo_path);
+        self.allocator.destroy(self);
+    }
+};
--- src/sqpack/sqpack.zig
+++ src/sqpack/sqpack.zig
@@ -4,6 +4,7 @@ const Allocator = std.mem.Allocator;
 
 const GameData = @import("../game_data.zig").GameData;
 const Repository = @import("repository.zig").Repository;
+const PathUtils = @import("path_utils.zig").PathUtils;
 
 pub const SqPack = struct {
     allocator: Allocator,
@@ -25,11 +26,47 @@ pub const SqPack = struct {
             .repos = .{},
         };
 
+        try self.scanForRepos();
+
         return self;
     }
 
     pub fn deinit(self: *SqPack) void {
+        self.cleanupRepos();
         self.allocator.free(self.repos_path);
         self.allocator.destroy(self);
+    }
+
+    fn scanForRepos(self: *SqPack) !void {
+        var folder = std.fs.openDirAbsolute(self.repos_path, .{ .iterate = true, .no_follow = true }) catch {
+            return error.InvalidGameFolder;
+        };
+        defer folder.close();
+
+        errdefer self.cleanupRepos(); // Cleanup on error
+
+        var walker = try folder.walk(self.allocator);
+        defer walker.deinit();
+        while (try walker.next()) |entry| {
+            if (entry.kind != std.fs.Dir.Entry.Kind.directory) continue;
+
+            const repo_name = entry.basename;
+            const repo_id = try PathUtils.repoNameToId(repo_name, false);
+
+            const repo_path = try std.fs.path.join(self.allocator, &.{ self.repos_path, repo_name });
+            defer self.allocator.free(repo_path);
+
+            const repo = try Repository.init(self.allocator, self, repo_path, repo_id);
+
+            try self.repos.put(self.allocator, repo_id, repo);
+        }
+    }
+
+    fn cleanupRepos(self: *SqPack) void {
+        for (self.repos.values()) |repo| {
+            repo.deinit();
+        }
+        self.repos.deinit(self.allocator);
+        self.repos = .{};
     }
 };
