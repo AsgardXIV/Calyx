81d3154133c77c0ec5b984637d1da6aad1e6a3f8
Asgard <git@asgard.io>
2025-04-06 17:01:38 -0600

    Make sure everything is exposed incase it's needed externall

--- src/game/sqpack.zig
+++ src/game/sqpack.zig
@@ -1,8 +1,13 @@
+pub const native_types = @import("sqpack/native_types.zig");
+pub const index = @import("sqpack/index.zig");
+
 pub const Category = @import("sqpack/Category.zig");
 pub const CategoryId = @import("sqpack/category_id.zig").CategoryId;
+pub const Chunk = @import("sqpack/Chunk.zig");
 pub const DatFile = @import("sqpack/DatFile.zig");
 pub const Pack = @import("sqpack/Pack.zig");
 pub const PackFileName = @import("sqpack/PackFileName.zig");
+pub const ParsedGamePath = @import("sqpack/ParsedGamePath.zig");
 pub const Repository = @import("sqpack/Repository.zig");
 pub const RepositoryId = @import("sqpack/repository_id.zig").RepositoryId;
-pub const ParsedGamePath = @import("sqpack/ParsedGamePath.zig");
+pub const ResolvedGameFile = @import("sqpack/ResolvedGameFile.zig");
