ae43d8877c7718e3668a06945eb326c50f273e45
Asgard <git@asgard.io>
2025-04-06 18:22:04 -0600

    Add autodocs

--- build.zig
+++ build.zig
@@ -31,6 +31,16 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
 
+    // Docs
+    const lib_install_docs = b.addInstallDirectory(.{
+        .source_dir = lib.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+
+    const docs_step = b.step("docs", "Install docs into zig-out/docs");
+    docs_step.dependOn(&lib_install_docs.step);
+
     // Sample executable
     if (build_sample) {
         const sample_exe_mod = b.createModule(.{
