44f2d246a2b9f87eac8e427deb589f27cab60df6
Asgard <git@asgard.io>
2025-04-07 13:24:26 -0600

    Missing export

--- src/game/excel.zig
+++ src/game/excel.zig
@@ -3,4 +3,5 @@ pub const native_types = @import("excel/native_types.zig");
 pub const ExcelDataFile = @import("excel/ExcelDataFile.zig");
 pub const ExcelHeader = @import("excel/ExcelHeader.zig");
 pub const ExcelList = @import("excel/ExcelList.zig");
+pub const ExcelSheetContainer = @import("excel/ExcelSheetContainer.zig");
 pub const ExcelSystem = @import("excel/ExcelSystem.zig");
