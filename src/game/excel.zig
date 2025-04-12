e9e7e83dd644b77dc360dc178bb614819dc448ab
Asgard <git@asgard.io>
2025-04-12 15:48:39 -0600

    Fix missing import

--- src/game/excel.zig
+++ src/game/excel.zig
@@ -4,4 +4,5 @@ pub const ExcelData = @import("excel/ExcelData.zig");
 pub const ExcelHeader = @import("excel/ExcelHeader.zig");
 pub const ExcelList = @import("excel/ExcelList.zig");
 pub const ExcelModule = @import("excel/ExcelModule.zig");
+pub const ExcelRawRow = @import("excel/ExcelRawRow.zig");
 pub const ExcelSheet = @import("excel/ExcelSheet.zig");
