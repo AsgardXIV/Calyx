450c3d4c3baab2b3e32946edaca3a00da190bbab
Asgard <git@asgard.io>
2025-04-14 22:20:36 -0600

    Bring sample and readme into line

--- README.md
+++ README.md
@@ -42,13 +42,21 @@ exe_mod.addImport("calyx", calyx_dependency.module("calyx"));
 
 3. Use the library
 ```zig
+ // Init Calyx
 const calyx = @import("calyx");
 const game = try calyx.GameData.init(allocator, .{});
 defer game.deinit();
+
+// Read a game file
+const swine_head_model = try game.getFileContents(allocator, "chara/equipment/e6023/model/c0101e6023_met.mdl");
+defer allocator.free(swine_head_model);
+try std.io.getStdOut().writer().print("Swine Head model length: {d} bytes\n", .{swine_head_model.len});
+
+// Read from Excel
 const sheet = try game.getSheet("Item");
 const wind_up_raha = try sheet.getRow(23992);
 const wind_up_raha_name = try wind_up_raha.getRowColumnValue(9);
-try std.io.getStdOut().writer().print("Item Name: {s}", .{wind_up_raha_name.string});
+try std.io.getStdOut().writer().print("Item Name: {s}\n", .{wind_up_raha_name.string});
 ```
 
 ## Developing Locally
--- sample/main.zig
+++ sample/main.zig
@@ -6,11 +6,18 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
+    // Init Calyx
     const game = try calyx.GameData.init(allocator, .{});
     defer game.deinit();
 
-    const sheet = try game.excel.getSheet("Item");
+    // Read a game file
+    const swine_head_model = try game.getFileContents(allocator, "chara/equipment/e6023/model/c0101e6023_met.mdl");
+    defer allocator.free(swine_head_model);
+    try std.io.getStdOut().writer().print("Swine Head model length: {d} bytes\n", .{swine_head_model.len});
+
+    // Read from Excel
+    const sheet = try game.getSheet("Item");
     const wind_up_raha = try sheet.getRow(23992);
     const wind_up_raha_name = try wind_up_raha.getRowColumnValue(9);
-    try std.io.getStdOut().writer().print("Item Name: {s}", .{wind_up_raha_name.string});
+    try std.io.getStdOut().writer().print("Item Name: {s}\n", .{wind_up_raha_name.string});
 }
