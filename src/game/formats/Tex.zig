a2ad926bb18194e1598fe2b1591e1a34ee5a9924
Asgard <git@asgard.io>
2025-04-17 00:11:23 -0600

    Fix some texture issues

--- src/game/formats/Tex.zig
+++ src/game/formats/Tex.zig
@@ -40,7 +40,7 @@ fn populate(tex: *Tex, fbs: *FixedBufferStream) !void {
 }
 
 pub const TexHeader = extern struct {
-    attribute: TextureAttribute align(1),
+    attributes: TextureAttribute align(1),
     format: TextureFormat align(1),
 
     width: u16 align(1),
@@ -73,22 +73,46 @@ pub const TextureAttribute = enum(u32) {
     texture_type_1d = 0x400000,
     texture_type_2d = 0x800000,
     texture_type_3d = 0x1000000,
+    texture_type_2d_array = 0x10000000,
     texture_type_cube = 0x2000000,
-    texture_type_mask = 0x3C00000,
+    texture_type_mask = 0x13C00000,
     texture_swizzle = 0x4000000,
     texture_no_tiled = 0x8000000,
     texture_no_swizzle = 0x80000000,
 
     _,
+
+    pub fn hasFlag(self: TextureAttribute, flag: TextureAttribute) bool {
+        return (@intFromEnum(self) & @intFromEnum(flag)) != 0;
+    }
+
+    pub fn hasAllFlags(self: TextureAttribute, flag: []TextureAttribute) bool {
+        for (flag) |f| {
+            if (!self.hasFlag(f)) {
+                return false;
+            }
+        }
+    }
+
+    pub fn hasAnyFlag(self: TextureAttribute, flag: []TextureAttribute) bool {
+        for (flag) |f| {
+            if (self.hasFlag(f)) {
+                return true;
+            }
+        }
+        return false;
+    }
 };
 
 pub const TextureFormat = enum(u32) {
     b4g4r4a4 = 0x1440,
+    b5g5r5a1 = 0x1441,
     b8g8r8a8 = 0x1450,
     bc1 = 0x3420, // DXT1
     bc2 = 0x3430, // DXT3
     bc3 = 0x3431, // DXT5
     bc5 = 0x6230, // ATI2
     bc7 = 0x6231,
+
     _,
 };
--- test/tests/tex.zig
+++ test/tests/tex.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 
 const calyx = @import("calyx");
 
+const Tex = calyx.game.formats.Tex;
+
 test "tex header" {
     std.log.info("Testing basic tex header", .{});
 
@@ -9,20 +11,52 @@ test "tex header" {
     defer game_data.deinit();
 
     {
-        const tex_file = try game_data.getTypedFile(std.testing.allocator, calyx.game.formats.Tex, "chara/equipment/e6202/texture/v01_c0101e6202_top_mask.tex");
+        const tex_file = try game_data.getTypedFile(std.testing.allocator, Tex, "chara/equipment/e6202/texture/v01_c0101e6202_top_mask.tex");
         defer tex_file.deinit();
 
         try std.testing.expectEqual(.bc1, tex_file.header.format);
         try std.testing.expectEqual(512, tex_file.header.width);
         try std.testing.expectEqual(1024, tex_file.header.height);
+        try std.testing.expect(tex_file.header.attributes.hasFlag(.texture_type_2d));
     }
 
     {
-        const tex_file = try game_data.getTypedFile(std.testing.allocator, calyx.game.formats.Tex, "chara/equipment/e6202/texture/v01_c0101e6202_top_id.tex");
+        const tex_file = try game_data.getTypedFile(std.testing.allocator, Tex, "chara/equipment/e6202/texture/v01_c0101e6202_top_id.tex");
         defer tex_file.deinit();
 
         try std.testing.expectEqual(.bc5, tex_file.header.format);
         try std.testing.expectEqual(512, tex_file.header.width);
         try std.testing.expectEqual(1024, tex_file.header.height);
+        try std.testing.expect(tex_file.header.attributes.hasFlag(.texture_type_2d));
+    }
+
+    {
+        const tex_file = try game_data.getTypedFile(std.testing.allocator, Tex, "ui/icon/002000/002660_hr1.tex");
+        defer tex_file.deinit();
+
+        try std.testing.expectEqual(.b5g5r5a1, tex_file.header.format);
+        try std.testing.expectEqual(80, tex_file.header.width);
+        try std.testing.expectEqual(80, tex_file.header.height);
+        try std.testing.expect(tex_file.header.attributes.hasFlag(.texture_type_2d));
+    }
+
+    {
+        const tex_file = try game_data.getTypedFile(std.testing.allocator, Tex, "ui/icon/060000/060223_hr1.tex");
+        defer tex_file.deinit();
+
+        try std.testing.expectEqual(.b8g8r8a8, tex_file.header.format);
+        try std.testing.expectEqual(64, tex_file.header.width);
+        try std.testing.expectEqual(64, tex_file.header.height);
+        try std.testing.expect(tex_file.header.attributes.hasFlag(.texture_type_2d));
+    }
+
+    {
+        const tex_file = try game_data.getTypedFile(std.testing.allocator, Tex, "ui/icon/214000/214925_hr1.tex");
+        defer tex_file.deinit();
+
+        try std.testing.expectEqual(.b4g4r4a4, tex_file.header.format);
+        try std.testing.expectEqual(48, tex_file.header.width);
+        try std.testing.expectEqual(64, tex_file.header.height);
+        try std.testing.expect(tex_file.header.attributes.hasFlag(.texture_type_2d));
     }
 }
